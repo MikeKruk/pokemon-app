189a82f05bfb5a5d6e6c5758a9d4e28608caaec8
Mikhail Kruk <mi.kruk95@gmail.com>
2026-03-28 18:12:48 +0100

    feat: add search input

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Header from './components/layout/Header';
 import PokemonCard from './components/layout/PokemonCard';
+import SearchInput from './components/layout/SearchInput';
 import TypeFilter from './components/layout/TypeFilter';
 import usePokemonDetails from './features/pokemon/hooks/usePokemonDetails';
 import usePokemonList from './features/pokemon/hooks/usePokemonList';
@@ -9,21 +10,23 @@ import type { PokemonListItem } from './types/pokemon';
 
 function App() {
 	const [selectedTypes, setSelectedTypes] = useState<string[]>([]);
+	const [search, setSearch] = useState<string>('');
 	const { data: pokemonList } = usePokemonList();
 	const names = pokemonList?.pages.flatMap(page =>
 		page.results.map((result: PokemonListItem) => result.name)
 	);
 	const { data: pokemon } = usePokemonDetails(names ?? []);
 	const { data: typesList } = usePokemonType();
-  
+
 	const types = typesList?.results.map((type: { name: string }) => type.name);
 
-	const filteredPokemon =
-		selectedTypes.length === 0
-			? pokemon ?? []
-			: (pokemon ?? []).filter(p =>
-					p.types.some(type => selectedTypes.includes(type.type.name))
-			  );
+	const filteredPokemon = (pokemon ?? []).filter(p => {
+		const searchMatch = p.name.toLowerCase().includes(search.toLowerCase());
+		const typeMatch =
+			selectedTypes.length === 0 ||
+			p.types.some(type => selectedTypes.includes(type.type.name));
+		return searchMatch && typeMatch;
+	});
 
 	function handelToggleType(type: string) {
 		setSelectedTypes(prev =>
@@ -35,11 +38,14 @@ function App() {
 		<div className='min-h-screen'>
 			<Header />
 			<main className='max-w-6xl mx-auto px-4 py-6'>
+				<SearchInput value={search} onChange={setSearch} />
+
 				<TypeFilter
 					types={types ?? []}
 					selected={selectedTypes}
 					onToggle={handelToggleType}
 				/>
+
 				<section className='grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-3'>
 					{filteredPokemon.map(pokemon => (
 						<PokemonCard
